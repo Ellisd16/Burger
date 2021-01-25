93d70c3aa8d4e8791f983cd8aa3ed9c40a40f493
Ellis Delgado <ellisdelgado13@gmail.com>
2021-01-25 13:57:36 -0800

    Push 0.5, completed burger_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,8 +4,29 @@ const router = express.Router();
 
 const express = require("express");
 
+//directs user to homepage
 router.get("/", (req, result) => {
-    burger.all(function (data) {
-
+    burger.selectAll(function (data) {
+        const hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+// takes user to list of burgers
+router.post("/burgers/create", (req, res) => {
+    burger.insertOne(
+        req.body, burger_name,
+        function () {
+            res.redirect("/index")
+        });
+});
+//takes user to modifier, then to udpated list 
+router.post("/burger/devoure/:id", () => {
+    burger.updateOne(req.params.id, function () {
+        res.redirect("/index")
     })
 })
+
+module.exports = router;

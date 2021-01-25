9f1df8651a02b4030553bf121526c242dab3fb09
Ellis Delgado <ellisdelgado13@gmail.com>
2021-01-25 15:55:31 -0800

    0.8 Added handlebars data, updated burgercontroller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,17 @@
-const burger = require("../models/burger");
-
-const router = express.Router();
 
 const express = require("express");
+const router = express.Router();
+
+const burger = require("../models/burger");
+
+
+router.get("/", (req, res) => {
+    res.redirect("/index")
+})
 
 //directs user to homepage
-router.get("/", (req, result) => {
-    burger.selectAll(function (data) {
+router.get("/index", (req, res) => {
+    burger.all(function (data) {
         const hbsObject = {
             burger: data
         };
@@ -16,17 +21,27 @@ router.get("/", (req, result) => {
 });
 // takes user to list of burgers
 router.post("/burgers/create", (req, res) => {
-    burger.insertOne(
-        req.body, burger_name,
-        function () {
-            res.redirect("/index")
+    burger.create(["burger_name"],
+        [req.body.burger_name],
+        function (result) {
+            res.json({ id: result.insertId });
         });
 });
 //takes user to modifier, then to udpated list 
-router.post("/burger/devoure/:id", () => {
-    burger.updateOne(req.params.id, function () {
-        res.redirect("/index")
-    })
+router.post("/burger/devour/:id", () => {
+    const condition = "id = " + req.params.id;
+
+    burger.update(
+        {
+            burger_name: req.body.burger_name
+        },
+        condition, function (result) {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    )
 })
 
 module.exports = router;

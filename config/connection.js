68f576495ce0e7857fe555c877e3aa04c98a06b2
Ellis Delgado <ellisdelgado13@gmail.com>
2021-02-08 13:07:22 -0800

    Changed the host on connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     port: 3306,
     user: "root",
     password: "gator",

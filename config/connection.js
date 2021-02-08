570296d375bfa218f979485be9ebeddd2568c31f
Ellis Delgado <ellisdelgado13@gmail.com>
2021-02-08 13:16:03 -0800

    More server changes

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
+    // host: "127.0.0.1",
+    // port: 3306,
     user: "root",
     password: "gator",
     database: "burgers_db"

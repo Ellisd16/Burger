0ac64a08b2088cd13e5435523fc9bd482d0df7f0
Ellis Delgado <ellisdelgado13@gmail.com>
2021-02-08 13:22:00 -0800

    Even more server changes

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    // host: "127.0.0.1",
-    // port: 3306,
+    host: "127.0.0.1",
+    port: 3306,
     user: "root",
     password: "gator",
     database: "burgers_db"
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
+    // if (err) {
+    //     console.error("error connecting: " + err.stack);
+    //     return;
+    // }
     console.log("connected as id " + connection.threadId);
 });
 

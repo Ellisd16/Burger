e81467d83c2db166b77e0d2f903fe683b99c7370
Ellis Delgado <ellisdelgado13@gmail.com>
2021-02-08 12:57:47 -0800

    Messing around with server for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "gator",
     database: "burgers_db"

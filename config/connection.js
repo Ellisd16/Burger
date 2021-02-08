2fdf0540fb559f2c273d0076221df315ca518e1e
Ellis Delgado <ellisdelgado13@gmail.com>
2021-02-08 13:40:50 -0800

    Changed the connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "gator",
-    database: "burgers_db"
-});
+let connection;
+if (process.env.JAWSBD_URL) {
+    connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "127.0.0.1",
+        port: 3306,
+        user: "root",
+        password: "gator",
+        database: "burgers_db"
+    });
+}
 
 connection.connect((err) => {
     if (err) {

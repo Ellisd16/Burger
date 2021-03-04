e1ba61850bb5849d872102e8c675646c5d0f2309
Ellis Delgado <ellisdelgado13@gmail.com>
2021-03-04 15:59:45 -0800

    removed code from json, put it in connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,18 @@ if (process.env.JAWSBD_URL) {
     connection = mysql.createConnection(process.env.JAWSBD_URL);
 } else {
     connection = mysql.createConnection({
-        // host: "127.0.0.1",
-        // port: 3306,
-        host: "localhost",
-        user: "root",
-        password: "gator",
-        database: "burgers_db"
+        // // host: "127.0.0.1",
+        // // port: 3306,
+        // host: "localhost",
+        // user: "root",
+        // password: "gator",
+        // database: "burgers_db"
+        username: "b2yrgmovvecg3opw",
+        pasword: "racz9emyq8u3mjdm",
+        database: "pil28yrqjt030gqg",
+        host: "pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        dialect: "mysql"
     });
 }
 // adding this code to see where my issue is coming from 
